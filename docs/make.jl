b5737e12a4966904c131bb13135c9446a4d0c7c2
claresinger <clareees@gmail.com>
2020-02-11 12:27:16 -0800

    remove .jl from Broken in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,10 @@
-using Documenter, Broken.jl
+using Documenter, Broken
 makedocs(
   sitename="Docs for Broken.jl",
   doctest = false,
   strict = false,
   format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true",),
-  modules = [Documenter, Broken.jl],
+  modules = [Documenter, Broken],
   clean = false,
   pages = Any[
     "Home" => "index.md",
