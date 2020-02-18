d7be6f72244640b7c06038aad6b8a2dc9266b387
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-18 19:28:17 +0000

    build based on a2d1ea9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Broken.jl-1","page":"Home","title":"Broken.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"This is my Broken.jl package for learning about how to do fancy things on github.","category":"page"}]
+}
